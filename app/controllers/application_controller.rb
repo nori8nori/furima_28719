dd69e5aa77c262724bef2942827704f2b6041612
nori8nori <69037486+nori8nori@users.noreply.github.com>
2020-08-28 18:55:22 +0900

    ログイン、新規登録画面遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,15 @@ class ApplicationController < ActionController::Base
 
   before_action :basic_auth
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+
   private
+
+  #ユーザーの名前/誕生日をDBに保存出来る様に設定
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:nickname, :lastname, :firstname, :lastname_kana, :firstname_kana, :birsday ])
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  #空の場合はDBに保存しないというバリデーション＋αの制約
+  with_options presence: true do
+    # validates :email, format: { with: /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/, message: "PC・携帯どちらでも可"}
+    validates :encrypted_password, format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6}\z/, message: "６文字以上の半角英数字"}    
+    validates :nickname, format: { with: /\A[ぁ-んァ-ン一-龥]\A[a-zA-Z0-9]+\z/, message: "例）Furima太郎"}
+    validates :lastname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "例）山田"}
+    validates :firstname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "例）隆太郎"}
+    validates :lastname_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "例）ヤマダ"}
+    validates :firstname_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "例）リクタロウ"}
+    validates :birthday 
+  end
+
+  #アソシエーション
+  has_many:items
+  has_many:profiles
+  
 end
