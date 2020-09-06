b0f1d53af2534aa07da2b3e892c69f6eb4e4288f
nori8nori <69037486+nori8nori@users.noreply.github.com>
2020-09-06 18:47:35 +0900

    エラーハンドリング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,8 +42,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
+    if @item.destroy
     redirect_to items_path
+    else
+      render :index
+    end
   end
 
   # プライベートメソッド
