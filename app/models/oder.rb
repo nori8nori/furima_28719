0a083e4fd4bb20035d1e62df9cd1a8cfe0ba6899
nori8nori <69037486+nori8nori@users.noreply.github.com>
2020-09-07 16:09:00 +0900

    アソシエーション＆バリデーション

--- app/models/oder.rb
+++ app/models/oder.rb
@@ -1,2 +1,24 @@
 class Oder < ApplicationRecord
+
+  # belongs_to_active_hashを使用するための記述
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  #アソシエーション
+  belongs_to :buy
+  belongs_to_active_hash :delivery_area
+
+  #バリデーション設定
+  with_options presence: true do
+    validates :postalcode
+    validates :edelivery_area_id
+    validates :municipalities
+    validates :address
+    # validates :building
+    validates :phone
+    validates :buy_id
+  end
+
+  # 選択が「--」の時は保存できないようにする
+  validates :delivery_area_id, numericality: { other_than: 0, message: 'Select' }
+
 end
