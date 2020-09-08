3e0b9b22c388b9c2e0e8d57a1f955914579526ef
nori8nori <69037486+nori8nori@users.noreply.github.com>
2020-09-08 19:54:54 +0900

    formオブジェクト-実行前-

--- app/controllers/oders_controller.rb
+++ app/controllers/oders_controller.rb
@@ -23,7 +23,7 @@ class OdersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = "sk_test_9a03947f622f7f086dbb083e"  # PAY.JPテスト秘密鍵
+    Payjp.api_key = "sk_test_f7f086dbb083e"  # PAY.JPテスト秘密鍵
     Payjp::Charge.create(
       amount: order_params[:price],  # 商品の値段
       card: order_params[:token],    # カードトークン
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_2cc5336c63785063eb7f34b4");// PAY.JPテスト公開鍵。注意）公開鍵を残したままGitHubにプッシュしてはいけない
+  Payjp.setPublicKey("pb7f34b4");// PAY.JPテスト公開鍵。注意）公開鍵を残したままGitHubにプッシュしてはいけない
   const form = document.getElementById("charge-form");//charge-form内のsubmitが実行されるとイベントが発火
   form.addEventListener("submit", (e) => {
     e.preventDefault();
