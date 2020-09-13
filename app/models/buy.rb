a871fbbd6c440be944ed30466bf0f38957f15723
nori8nori <69037486+nori8nori@users.noreply.github.com>
2020-09-13 12:10:39 +0900

    商品出品機能-修正２-

--- app/models/buy.rb
+++ app/models/buy.rb
@@ -2,12 +2,4 @@ class Buy < ApplicationRecord
   has_one :oder
   belongs_to :item
   belongs_to :user
-
-  # バリデーション設定
-  # with_options presence: true do
-
-  #   validates :user_id
-  #   validates :item_id
-
-  # end
 end
